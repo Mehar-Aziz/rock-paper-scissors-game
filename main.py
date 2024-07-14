14d042a1cb78e5662f9c4240b3a12adb618d940a
Mehar-Aziz <meharaziz.1614>
2024-07-14 15:15:02 +0500

    rock paper and scissors game

--- main.py
+++ main.py
@@ -26,3 +26,23 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+game_images = [rock, paper, scissors]
+user_choice = int(input("What do you want to choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print(game_images[user_choice])
+
+computer_choice = random.randint(0, 2)
+print("Computer Chose: ")
+print(game_images[computer_choice])
+
+if user_choice>=3 or user_choice<0:
+    print("You typed an invalid number")
+elif user_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+    print("You win!")
+elif computer_choice > user_choice:
+    print("You lose!")
+elif computer_choice < user_choice:
+    print("You win!")
+elif computer_choice == user_choice:
+    print("It's a Draw")
