c18c2e869c5b49e7bea24003be1db71f887ec873
Mehar-Aziz <meharaziz.1614>
2024-07-14 14:16:56 +0500

    update code

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import random
-
-random_integer = random.randint(1, 10) #for integer
-print(random_integer)
-#0.000000 - 0.999999
-random_float = random.random()*5
-print(random_float)
+cities_of_pakistan = ["Lahore", "Karachi", "Islamabad", "Badin", "Quetta"]
+list = len(cities_of_pakistan)
+print(cities_of_pakistan[list-1])
