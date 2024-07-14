6be5488a385e50239604e5fe7d1b6d415f789fb5
Mehar-Aziz <meharaziz.1614>
2024-07-14 14:23:54 +0500

    dirty dozen

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
-cities_of_pakistan = ["Lahore", "Karachi", "Islamabad", "Badin", "Quetta"]
-list = len(cities_of_pakistan)
-print(cities_of_pakistan[list-1])
+# dirty_dozen = [
+#     "Strawberries",
+#     "Spinach",
+#     "Kale",
+#     "Peaches",
+#     "Pears",
+#     "Nectarines",
+#     "Apples",
+#     "Grapes",
+#     "Bell and Hot Peppers",
+#     "Cherries",
+#     "Blueberries",
+#     "Green Beans"
+# ]
+fruites = [
+    "Strawberries",
+    "Peaches",
+    "Pears",
+    "Nectarines",
+    "Apples",
+    "Grapes",
+    "Cherries",
+    "Blueberries"
+]
+
+vegetables = [
+    "Spinach",
+    "Kale",
+    "Bell and Hot Peppers",
+    "Green Beans"
+]
+
+dirty_dozen = [fruites, vegetables]
+print(dirty_dozen)
