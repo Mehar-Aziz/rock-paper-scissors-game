cd6015f2d7feb0727877ac6e12a79587a0096834
Mehar-Aziz <meharaziz.1614>
2024-07-14 13:18:18 +0500

    ramdamization

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import random
 
-random_integer = random.randint(1, 10)
+random_integer = random.randint(1, 10) #for integer
 print(random_integer)
+#0.000000 - 0.999999
+random_float = random.random()*5
+print(random_float)
